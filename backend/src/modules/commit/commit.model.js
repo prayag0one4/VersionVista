a71ae2f3728a908354d8ba19c291158b77e2bd8e
Ayush-1812 <2023kucp1053@iiitkota.ac.in>
2026-04-14 02:29:01 +0530

    integrated simple git + data models

--- backend/src/modules/commit/commit.model.js
+++ backend/src/modules/commit/commit.model.js
@@ -4,28 +4,53 @@ const commitSchema = new mongoose.Schema({
   repoId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Repo",
-    required: true
+    required: true,
+    index: true   
   },
+
   commitHash: {
     type: String,
+    required: true,
+    unique: true    
+  },
+
+  author: {
+    name: { type: String, trim: true },
+    email: { type: String, trim: true }
+  },
+
+  message: {
+    type: String,
+    trim: true
+  },
+
+  timestamp: {
+    type: Date,
     required: true
   },
-  author: {
-    name: String,
-    email: String
-  },
-  message: String,
-  timestamp: Date,
+
   parentHash: String,
 
-  filesChanged: Number,
-  insertions: Number,
-  deletions: Number,
+  filesChanged: {
+    type: Number,
+    default: 0
+  },
+
+  insertions: {
+    type: Number,
+    default: 0
+  },
+
+  deletions: {
+    type: Number,
+    default: 0
+  },
 
   processed: {
     type: Boolean,
     default: false
   }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Commit", commitSchema);
